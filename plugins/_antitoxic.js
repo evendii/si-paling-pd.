63fe37609060a0c51b74895294f2fc5675e98e06
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-06-17 18:50:18 +0800

    HN
    HN

--- plugins/_antitoxic.js
+++ plugins/_antitoxic.js
@@ -13,9 +13,10 @@ export async function before(m, { conn, isAdmin, isBotAdmin }) {
             if (m.text.includes('Asu')) return !0
             if (m.text('Anj')) return !0
             if (m.text('Ajg')) return !0
-            if (m.text('Memek')) return !0
+            if (m.text == 'Memek') return !0
             if (m.text('Gblk')) return !0
             if (m.text('Tolol')) return !0
+            if (m.text.includes("Anjing","Bapakao","Tulul")) return !0
         }
         await conn.sendButton(m.chat, `*Kata Aneh Terdeteksi!*${isBotAdmin ? '' : '\n\n_Bot bukan admin_'}`, author, ['off antitoxic', '/disable antitoxic'], m)
         if (isBotAdmin && bot.restrict) {
