ecd1bb7ab59af99e31cecdbddeef6745909dd86e
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-06-17 19:11:40 +0800

    HN
    HN

--- plugins/_antitoxic.js
+++ plugins/_antitoxic.js
@@ -5,17 +5,16 @@ export async function before(m, { conn, isAdmin, isBotAdmin }) {
     if (!m.isGroup) return !1
     let chat = global.db.data.chats[m.chat]
     let bot = global.db.data.settings[this.user.jid] || {}
-    const isAntiToxic = m.text
-
-    if (chat.antiToxic && isAntiToxic && !isAdmin) {
+    
+    if (chat.antiToxic && !isAdmin) {
         if (isBotAdmin) {
-            if (m.sender.startsWith('Kontol')) return !0
-            if (m.text.includes('Asu')) return !0
-            if (m.text('Anj')) return !0
-            if (m.text('Ajg')) return !0
+            if (m.text == 'Kontol') return !0
+            if (m.text == 'Asu') return !0
+            if (m.text == 'Anj') return !0
+            if (m.text == 'Ajg') return !0
             if (m.text == 'Memek') return !0
-            if (m.text('Gblk')) return !0
-            if (m.text('Tolol')) return !0
+            if (m.text == 'Gblk') return !0
+            if (m.text == 'Tolol') return !0
             if (m.text.includes("Anjing","Bapakao","Tulul")) return !0
         }
         await conn.sendButton(m.chat, `*Kata Aneh Terdeteksi!*${isBotAdmin ? '' : '\n\n_Bot bukan admin_'}`, author, ['off antitoxic', '/disable antitoxic'], m)
--- plugins/_antivirus.js
+++ plugins/_antivirus.js
@@ -5,14 +5,12 @@ export async function before(m, { conn, isAdmin, isBotAdmin }) {
     if (!m.isGroup) return !1
     let chat = global.db.data.chats[m.chat]
     let bot = global.db.data.settings[this.user.jid] || {}
-    const isAntiVirtex = m.text
-
-    if (chat.antiVirtex && isAntiVirtex && !isAdmin) {
+    
+    if (chat.antiVirtex && !isAdmin) {
         if (isBotAdmin) {
-            if (m.text('﻿Đ.Δ.Μ.Ň.į.Ҝ.Δ')) return !0
-            if (m.text('Bot Kontol')) return !0
-            if (m.text('๑๑๑' || '๑๑๑')) return !0
-            if (m.text('๒๒๒' || '๒๒๒')) return !0
+            if (m.text == '๒๒๒') return !0
+            if (m.text == '๑๑๑') return !0
+            if (m.text == 'Đ.Δ.Μ.Ň.į.Ҝ.Δ') return !0
             if (!m.isBaileys && m.text.length > 384) return !0
             if (m.text && m.text.length >= 25000) return !0
             if (m.messageStubType === 68) return !0
