efa4576f98bd9ef2196b57d5510b23c880c6bf25
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-06-20 07:49:47 +0800

    HN
    HN

--- plugins/ini_sfx.js
+++ plugins/ini_sfx.js
@@ -15,42 +15,8 @@ let json = await fetch(`http://www.myinstants.com/api/v1/instants/?format=json&p
         return m.reply(caption)
 }
 
-if (command == 'sfx2') {
-if (!text) throw `Contoh:
-${usedPrefix + command} 10`
-let gas = await fetch(`http://www.myinstants.com/api/v1/instants/?format=json&page=${text}`)
-    let json = await gas.json()
-
-for (var i=0; i<json.results.length; i++) {
-       let tor = json.results[i].name
-       let ter = json.results[i].sound
-
-let nm = 1
-let nm2 = 0
-for (let x of ter) {
-const yy = {
-title: 'Sound ke -'+ nm++,
-rows: [
-{title: `${tor[nm2++]}`,
- rowId: `${usedPrefix}get ${x}`}
- ]
 }
-let h = []
-let sections = h.push(yy)
-
-const LiM = {
-  text: `⚡ Silakan pilih opsi di tombol di bawah...`,
-  footer: global.wm,
-  title: `⎔───「 ${command} 」───⎔`,
-  buttonText: `☂️ Tema Disini ☂️`,
-  sections
-}
-conn.sendMessage(m.chat, LiM, m)
-}}
-}
-
-}
-handler.command = handler.help = ['sfx', 'sfx2']
+handler.command = handler.help = ['sfx']
 handler.tags = ['random']
 
 export default handler
