b5f7f7017ad507b353af947cb5c66929abdd48bd
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-06-21 00:43:46 +0800

    HN
    HN

--- plugins/ini-sound.js
+++ plugins/ini-sound.js
@@ -1,111 +1,102 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 if (!text) throw `Contoh:
 ${usedPrefix + command} 2
-Max Angka 70
-*Hanya Angka*`
-//LIST
-var lm = [
-'anjay',
-'ara-ara',
-'ara-ara-cowok',
-'ara-ara2',
-'arigatou',
-'assalamualaikum',
-'asu',
-'ayank',
-'aku-ngakak',
-'bacot',
-'bahagia-aku',
-'baka',
-'bansos',
-'beat-box',
-'beat-box2',
-'biasalah',
-'bidadari',
-'bot',
-'buka-pintu',
-'canda-anjing',
-'cepetan',
-'cuekin-terus',
-'daisuki-dayo',
-'daisuki',
-'dengan-mu',
-'gaboleh-gitu',
-'gak-lucu',
-'gamau',
-'gay',
-'gelay',
-'gitar',
-'gomenasai',
-'hai-bot',
-'hampa',
-'hayo',
-'hp-iphone',
-'i-like-you',
-'ih-wibu',
-'india',
-'karna-lo-wibu',
-'kiss',
-'kontol',
-'ku-coba',
-'maju-wibu',
-'makasih',
-'mastah',
-'nande-nande',
-'nani',
-'ngadi-ngadi',
-'nikah', 'nuina',
-'onichan',
-'owner-sange',
-'ownerku',
-'pak-sapardi',
-'pale',
-'pantek',
-'pasi-pasi',
-'punten',
-'sayang',
-'siapa-sih',
-'sudah-biasa',
-'summertime',
-'tanya-bapak-lu',
-'to-the-bone',
-'wajib',
-'waku',
-'woi',
-'yamete',
-'yowaimo',
-'yoyowaimo'
-]
-var nm = 1
-var nm2 = 0
-for (let x of lm) {
-var yy = {title: 'Sound ke -'+ nm++,
-rows: [
-{
-title: `${lm[nm2++]}`,
-description: `Gwejh mau sound yang ini`,
-rowId: `/get https://raw.githubusercontent.com/saipulanuar/Api-Github/main/audio/${x}.mp3`
-}
-]
-}
-var h = []
-var sections = h.push(yy)
-var listM = {
-  text: "Pilih Sound Dibawah",
-  footer: wm,
-  title: "O P T I O N",
-  buttonText: "Klik disini",
-  sections
-}
-await conn.sendMessage(m.chat, listM, m)
-}
 
-//VN
+List Number
+Max Angka 70
+
+Contoh:
+${usedPrefix + command} arigatou
+List Alphabet
+• anjay
+• ara-ara
+• ara-ara-cowok
+• ara-ara2
+• arigatou
+• assalamualaikum
+• asu
+• ayank
+• aku-ngakak
+• bacot
+• bahagia-aku
+• baka
+• bansos
+• beat-box
+• beat-box2
+• biasalah
+• bidadari
+• bot
+• buka-pintu
+• canda-anjing
+• cepetan
+• cuekin-terus
+• daisuki-dayo
+• daisuki
+• dengan-mu
+• gaboleh-gitu
+• gak-lucu
+• gamau
+• gay
+• gelay
+• gitar
+• gomenasai
+• hai-bot
+• hampa
+• hayo
+• hp-iphone
+• i-like-you
+• ih-wibu
+• india
+• karna-lo-wibu
+• kiss
+• kontol
+• ku-coba
+• maju-wibu
+• makasih
+• mastah
+• nande-nande
+• nani
+• ngadi-ngadi
+• nikah 
+• nuina
+• onichan
+• owner-sange
+• ownerku
+• pak-sapardi
+• pale
+• pantek
+• pasi-pasi
+• punten
+• sayang
+• siapa-sih
+• sudah-biasa
+• summertime
+• tanya-bapak-lu
+• to-the-bone
+• wajib
+• waku
+• woi
+• yamete
+• yowaimo
+• yoyowaimo`
+//LIST
+try {
+//VN 1
 let vn = `https://hansxd.nasihosting.com/sound/sound${text}.mp3`
 await conn.sendFile(m.chat, vn, 'song.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
 })
+} catch {
+//VN 2
+let vn = `https://raw.githubusercontent.com/saipulanuar/Api-Github/main/audio/${text}.mp3`
+await conn.sendFile(m.chat, vn, 'song.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true 
+})
+} catch (e) {
+return m.reply('Error')
+}
 
 }
 handler.help = ['sound <angka>']
