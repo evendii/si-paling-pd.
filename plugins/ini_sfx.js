b7a6d10cc7edc4416427b44c9e1f86d3c8ff7749
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-06-20 12:39:14 +0800

    HN
    HN

--- plugins/ini_sfx.js
+++ plugins/ini_sfx.js
@@ -6,28 +6,23 @@ if (!text) throw `Contoh:
 ${usedPrefix + command} 10`
 let json = await fetch(`http://www.myinstants.com/api/v1/instants/?format=json&page=${text}`)
         let jsons = await json.json()
-        let caption = '*⎔┉━「 Search 」━┉⎔*'
+        let caption = '*⎔┉━「 Search 」━┉⎔*\n'
         for (let x of jsons.results) {
-        caption += `
-        *Name :* ${x.name}
+        caption += `*Name :* ${x.name}
 *Sound :* ${x.sound}
 `}
         return m.reply(caption)
 }
 
 if (command == 'sfx2') {
-if (!text) throw `Contoh penggunaan ${usedPrefix}${command} 2|2`
-let urut = text.split`|`
-  let text1 = urut[0]
-  let text2 = urut[1]
-  
-    let gas = await fetch(`http://www.myinstants.com/api/v1/instants/?format=json&page=${text1}`)
+if (!text) throw `Contoh penggunaan ${usedPrefix}${command} 2`
+    let gas = await fetch(`http://www.myinstants.com/api/v1/instants/?format=json&page=${text}`)
     let json = await gas.json()
     let ter = json.results
 
     for (let i = 0; i < ter.length; i++) {
-        let out = ter[i][text2].sound
-        conn.sendFile(m.chat, out, 'song.mp3', null, m, true, {
+        let out = ter[i].sound
+        await conn.sendFile(m.chat, out, 'song.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
 })
