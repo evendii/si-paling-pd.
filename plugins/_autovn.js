452fc50f0f43d97559407d81e2c4a6eb84c09250
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-06-20 23:57:43 +0800

    HN
    HN

--- plugins/_autovn.js
+++ plugins/_autovn.js
@@ -8,7 +8,9 @@ if (m.isBaileys && m.fromMe)
     let chat = global.db.data.chats[m.chat]
     let bot = global.db.data.settings[this.user.jid] || {}
     if (chat.autoVn) {
-        let api = `https://hadi-api.herokuapp.com/api/tts?language=id&text=${m.text}`
-        conn.sendMessage(m.chat, { audio: { url: api }, mimetype: 'audio/mp4' })
+    let sim = await fetch(`https://api.simsimi.net/v2/?text=${m.text}&lc=id`)
+    let res = await sim.json()
+        let tts = `https://hadi-api.herokuapp.com/api/tts?language=id&text=${res.success}`
+        conn.sendMessage(m.chat, { audio: { url: tts }, mimetype: 'audio/mp4' })
     }
 }
