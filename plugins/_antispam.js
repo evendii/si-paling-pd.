27c0f63ec9e10631dc528ecd8ef5a1a5506c1e73
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-06-21 20:35:24 +0800

    HN
    HN

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -1,12 +1,4 @@
-
-export async function before(m, { conn, args, usedPrefix, command, isAdmin, isBotAdmin }) {
-    if (m.isBaileys && m.fromMe)
-        return !0
-    if (!m.isGroup) return !1
-    let chat = global.db.data.chats[m.chat]
-    let bot = global.db.data.settings[this.user.jid] || {}
-
-    if (chat.antiSpam) {
+export async function before(m) {
     if (!m.message)
         return
     this.spam = this.spam ? this.spam : {}
@@ -15,7 +7,7 @@ export async function before(m, { conn, args, usedPrefix, command, isAdmin, isBo
         if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 10) {
             if (this.spam[m.sender].count > 10) {
                 //global.db.data.users[m.sender].banned = true
-                await conn.sendButton(m.chat, `*Kata Spam Terdeteksi!*${isBotAdmin ? '' : '\n\n_Bot bukan atmin_'}`, author, ['off antispam', '/disable antispam'], m)
+                m.reply('*Jangan Spam!!*')
             }
             this.spam[m.sender].count = 0
             this.spam[m.sender].lastspam = m.messageTimestamp.toNumber()
@@ -27,19 +19,4 @@ export async function before(m, { conn, args, usedPrefix, command, isAdmin, isBo
             count: 0,
             lastspam: 0
         }
-        
-        if (isBotAdmin && bot.restrict) {
-            // await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-    global.db.data.users[m.sender].limit = 0
-    
-    await conn.sendButton(m.chat, `*Limit anda direset ke 0*
-    
-    Karena Menggunakan Kata Spam
-    
-    Ketik *.limit* untuk cek limit`, wm, null, [
-        ['Ngechit', `${usedPrefix}ngechit`]
-    ], m)
-        } else if (!bot.restrict) return m.reply('Gk bisa gw kick!')
-    }
-    return !0
 }
